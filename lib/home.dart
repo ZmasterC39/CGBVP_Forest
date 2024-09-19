a35edfa7d5bc2e736e464148c70e30de6e5722f9
jmontesc18_1 <jmontesc18_1@unc.edu.pe>
2024-09-19 19:15:06 +0000

    comit 2

---
+++ lib/home.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Página Principal"),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            ElevatedButton(
+              onPressed: () {
+                Navigator.pushNamed(context, '/counter');
+              },
+              child: const Text("Ir al Contador"),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.pushNamed(context, '/notifications');
+              },
+              child: const Text("Notificaciones"),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class NotificacionesPage extends StatelessWidget {
+  const NotificacionesPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Notificaciones"),
+      ),
+      body: const Center(
+        child: Text("Página de Notificaciones"),
+      ),
+    );
+  }
+}
